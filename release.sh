4c5659d27743c9fb4cb8e5f6ae1c684d40cf8d5c
darrellb <darrellb@metrumrg.com>
2019-10-11 16:18:26 -0400

    Updating release scripts to facilitate builds

--- release.sh
+++ release.sh
@@ -4,5 +4,5 @@ IMAGE=dukeofubuntu/goreleaser
 DIRECTORY=/root/go/github.com/metrumresearchgroup/focal
 
 #Githubtoken should come from env of whoever is running
-docker run --rm -it -v $(pwd):$DIRECTORY -w $DIRECTORY -e GITHUB_TOKEN=$GITHUB_TOKEN dukeofubuntu/goreleaser goreleaser --snapshot --skip-publish --rm-dist
+docker run --rm -it -v $(pwd):$DIRECTORY -w $DIRECTORY -e GITHUB_TOKEN=$GITHUB_TOKEN dukeofubuntu/goreleaser goreleaser --rm-dist
 
