19b15ea182a8505652551f9aaf61c1e2b791ef84
darrellb <darrellb@metrumrg.com>
2019-11-20 12:20:35 -0500

    Updating with initd script. Now verifiying that environment values are being passed

--- system/postinstall.sh
+++ system/postinstall.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-#Change ownership of config files
+#Change ownership of focal configs
 
 chown -R focal /etc/focal/*
 
-systemctl enable focal
-systemctl daemon-reload
-systemctl start focal
+#IS THIS SYSTEMD?
+which systemctl
+
+if [ $? -lt 1 ] ;
+    then
+    #this is a system D Box
+    echo "This is a System D Service"
+    #We've already copied the service file into the system. Let's reload, enable and start
+    systemctl daemon-reload
+    systemctl enable focal
+    systemctl start focal
+    exit 0 
+
+    else
+    #This is an upstart box
+    echo "This is an init.d system"
+    #Make sure the file is executable
+    chmod +x /etc/init.d/focal
+    update-rc.d focal defaults
+    service focal start
+    exit 0
+fi
